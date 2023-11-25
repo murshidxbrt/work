fd41adb8ebc1a1b4c0639d1c1557a2e239ca14ad
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2023-11-25 14:12:06 +0530

    models update

--- ecommerce-yt/models/models.go
+++ ecommerce-yt/models/models.go
@@ -7,11 +7,11 @@ import (
 )
 
 type User struct{
-	ID 							primitive.ObjectID				`jason:"_id"bson:"_id `
-	First_Name 					*string 						`jason:"First_Name" `			
-	Last_Name 					*string							`jason:"Last_Name" `
-	Password					*string							`jason:"Password" `
-	Email 						*string							`jason:"Email" `
+	ID 							primitive.ObjectID				`jason:"_id"bson:"_id `				
+	First_Name 					*string 						`jason:"First_Name" 			validate:"required,min=2,max=30" `
+	Last_Name 					*string							`jason:"Last_Name"              validate:"required,min=2,max=30" `
+	Password					*string							`jason:"Password"               validate:"required,min=6" `
+	Email 						*string							`jason:"Email"                  validate:"email, required"`
 	Phone						*string							`jason:"Phone"' `
 	Token						*string							`jason:"Token" `
 	Refresh_Token 		        *string							`jason:"Refresh_Token"`
@@ -24,35 +24,35 @@ type User struct{
 }
 
 type Product struct{
-	Adress_Id					primitive.ObjectID
-	Product_Name 				*string
-	Price 						*uint64
-	Rating						*uint8
-	Image 						*string
+	Adress_Id					primitive.ObjectID				`bson:"_id" `
+	Product_Name 				*string							`json:"product_name"`
+	Price 						*uint64							`jason:"price`
+	Rating						*uint8							`jason:"rating`
+	Image 						*string							`json:"image"`
 }
 type ProductUser struct{
-	Prioduct_ID					primitive.ObjectID
-	Product_Name 				*string
-	Price 						*uint64
-	rating						*uint8	
-	Image 						*string
+	Prioduct_ID					primitive.ObjectID				`bson:"_id"`
+	Product_Name 				*string							`json:"product_name" bson :"product_name"`
+	Price 						*int							`json:"price" bson:"price"`
+	Rating						*uint							`json:"rating" bson:"rating"`
+	Image 						*string							`json:"image" bson:"image"`
 
 }
 type Address struct {
-	Address_Id					*string
-	House                       *string
-	Street                      *string	
-	City						*string
-	Pincode 					*string
+	Address_Id					primitive.ObjectID              `bson:"_id"`
+	House                       *string							`json:"house_name" bson:"house_name"`
+	Street                      *string							`json:"street_name" bson:"street_name"`		
+	City						*string							`json:"city_name" bson:"city_name"`
+	Pincode 					*string							`json:"pinc_ode" bson:"pin_code"`
 
 }
 type Order struct{
-	Order_Id					primitive.ObjectID
-	Order_Cart					[]ProductUser
-	Order_At 					time.Time
-	Price						int
-	Discount					*int
-	Payment_Method				Payment
+	Order_Id					primitive.ObjectID				`bson:"_id"`
+	Order_Cart					[]ProductUser					`json:"order_list" bson:"order_list"`
+	Order_At 					time.Time						`json:"ordered_at" bson:"ordered_at"`
+	Price						int								`json:"total_price" bson:"total_price"`
+	Discount					*int							`json:"discount" bson:"discount"`
+	Payment_Method				Payment							`json:"payment_method" bson:"payment`
 
 }
 type Payment struct {
