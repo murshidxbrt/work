5321075bcb5f527faa79c460ea492a3dc37363c3
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2023-11-25 14:29:32 +0530

    adress func update

---
+++ ecommerce-yt/controllers/address.go
@@ -0,0 +1,33 @@
+package controllers
+
+import(
+
+)
+
+func HashPassword (password string) string{
+
+}
+
+func VerifyPassword (userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func Signup () gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+	
+}
+
+func ProductViewerAdmin() gin.handlerFunc{
+
+}
+
+func searchProduct() gin.HandlerFunc {
+
+}
+
+func searchProductByQuery() gin.HandlerFunc {
+	
+}
