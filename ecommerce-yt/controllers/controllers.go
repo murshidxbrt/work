74bc0bded92aabeb31bc12d7f7f0da695d358bd8
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2023-11-27 16:47:45 +0530

    update

--- ecommerce-yt/controllers/controllers.go
+++ ecommerce-yt/controllers/controllers.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"honnef.co/go/tools/analysis/facts/generated"
 )
 
 func HashPassword (password string) string{
@@ -52,12 +55,69 @@ func Signup () gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error" : "this phone no. is already in use"})
 			return
 		}
+		password := HashPassword("user.Password")
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User = user.ID.hex()
+		token, refreshtocken,  _ := generate.TockenGenerator(*user.Email, *user.First_Name, *user.Last_Name, *user.User_Id)
+		user.Token = &token
+		user.Refresh_Token = &refreshtocken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		 if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"the user did not get created"})
+			return
+		 }
+		 defer cancel()
+		 c.JSON(http.StatusCreated, "Successfully Signed in !")
+
 
 	}
 
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var func (c *gin.Context){
+			var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+			defer cancel()
+
+			var user models.User
+			if err := c.BindJSON(&user); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H {"error": err})
+				return
+			}
+
+			err := UserCollection.FindOne(ctx ,bson.M{"email": user.Email}).Decode(&founduser)
+			defer cancel()
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+				return
+		
+			}
+			PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+
+			defer cancel()
+
+			if !PasswordIsValid{
+				c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+				fmt.Println(msg)
+				return
+			}
+			token, refreshToken, _ := generate.TockenGenerator(*founduser.Email, *founduser.First_Name, *founduser.Last_Name, *founduser.User_ID)
+			defer cancel()
+
+			generate.UpdateAllTockens(token, refreshToken, *founduser.User_ID)
+			c.JSON(http.Statusfound, founduser)
+
+
+	}
 	
 }
 
