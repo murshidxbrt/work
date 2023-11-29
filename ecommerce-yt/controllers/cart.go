a53a6ea1f4e2a0f4a731ba57432444f3e92b1ddd
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2023-11-29 12:51:52 +0530

    #

--- ecommerce-yt/controllers/cart.go
+++ ecommerce-yt/controllers/cart.go
@@ -60,7 +60,45 @@ func (App *Appliction) AddCart() gin.Handler {
 
 }
 
-func RemoteIteam() gin.HandlerFunc {
+func (app *Appliction) RemoveIteam() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is empty")
+
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return
+		}
+		userQueryID := c.Query("user_ID")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+			return
+	    }
+		productID , err := primitive.ObjectIDFromHex(productQueryID) 
+
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return 
+
+		}
+		
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+
+		defer cancel()
+
+		err = database.RemoveCartIteam(ctx, app.prodCollection, app.userCollection, productID, productQueryID)
+
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+			return
+		}
+		c.IndentedJSON(200, "Successfully removed iteam from cart")
+
+
+	}
 
 }
 
