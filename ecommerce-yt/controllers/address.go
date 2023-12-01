ece697fe4dd6b543fe5b633dbaad6cfbcfa45335
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2023-12-01 22:12:01 +0530

    # address

--- ecommerce-yt/controllers/address.go
+++ ecommerce-yt/controllers/address.go
@@ -17,5 +17,15 @@ func EditWorkAddress() gin.HandlerFunc{
 }
 
 func DeleteAddress() gin.HandlerFunc{
+	return func (c *gin.Context) {
+		user_id := c.Query("id")
+
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound,  gin.H{"Error":"Invalid Search Index"})
+			c.Abort()
+			return
+		}
+	}
 
 }
