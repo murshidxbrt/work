2eb08cd14ed1c97d945ce4de1b9dfffe87a24e90
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2024-01-03 16:12:16 +0530

    #

--- ecommerce-yt/controllers/address.go
+++ ecommerce-yt/controllers/address.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/murshidxbrt/ecommerce-yt/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -85,16 +86,56 @@ func EditHomeAddress() gin.HandlerFunc{
 		if err != nil {
 			c.IndentedJSON(500, "Internal server error")
 		}
+		var editaddress models.Address
+		if err := c.BindJSON(&editaddress); err != nil{
+			c.IndentedJSON(http.StatusBadRequest, err.Error())
+		}
 		var ctx, cancel = context.WithTimeout(context.Background(),100 * time.Second)
 		defer cancel()
-		filter := bson.D{primitive.E(key:"_id", Value: user_id)}
-		update := bson.D{{Key:"$set", Value:bson.D{primitive.E{Key:"address.0.house_name", Value: editaddress.House},{Key:"address.0.street_name", Value: editaddress.Street}}}}
-
+		filter := bson.D{primitive.E{key:"_id", Value: usert_id}}
+		update := bson.D{{Key:"$set", Value:bson.D{primitive.E{Key:"address.0.house_name", Value: editaddress.House},{Key:"address.0.street_name", Value: editaddress.Street},{Key:"address.0.city_name", Value: Editaddress.City},{Key:"address.0.pin_code", Value: editadress.Pincode}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(500, "Something went wrong")
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200,"successfully updated the home address")
 	}
 
 }
 
 func EditWorkAddress() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid"})
+			c.Abort()
+			return
+		}
+		usert_id, err := primitive.ObjectIDFromhex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+		var editaddress models.Address
+		if err := c.BindJSON(&editaddress); err != nil{
+			c.IndentedJSON(http.StatusBadRequest, err.Error())
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(),100 * time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{key:"_id", Value: usert_id}}
+		update := bson.D{{Key:"$set", Value:bson.D{primitive.E{Key:"address.1.house_name", Value: editaddress.House},{Key:"address.1.street_name", Value: editaddress.Street},{Key:"address.1.city_name", Value: Editaddress.City},{Key:"address.1.pin_code", Value: editadress.Pincode}}}}
+		_,err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(500, "something went wrong")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Sucessfully updated the work address")
+
+	}
 
 }
 
