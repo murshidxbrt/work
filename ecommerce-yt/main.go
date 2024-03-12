c123f707e9fa442293b8bb7d454d05722d9e8145
Muhammed Murshid T B <59514436+murshidxbrt@users.noreply.github.com>
2024-03-12 23:41:08 +0530

    #

--- ecommerce-yt/main.go
+++ ecommerce-yt/main.go
@@ -1,38 +1,36 @@
 package main
 
-import(
+import (
 	"log"
 	"os"
 
-	"github.com/murshidxbrt/ecommerce-yt/controllers"
-	"github.com/murshidxbrt/ecommerce-yt/database"
-	"github.com/murshidxbrt/ecommerce-yt/middleware"
-	"github.com/murshidxbrt/ecommerce-yt/routes"
 	"github.com/gin-gonic/gin"
-	
+	"github.com/murshidxbrt/work/ecommerce-yt/controllers"
+	"github.com/murshidxbrt/work/ecommerce-yt/database"
+	"github.com/murshidxbrt/work/ecommerce-yt/middleware"
+	"github.com/murshidxbrt/work/ecommerce-yt/routes"
 )
 
 func main() {
 
-	port :=os.Getenv("PORT")
-	  if port == "" {
+	port := os.Getenv("PORT")
+	if port == "" {
 		port = "8080"
-	  }
-	  
-	  app := controllers.NewAppliction(database.ProductData(database.Client, "Products"),database.UserData(database.Client, "users"))
-
-	  router := gin.New()
-	  router.Use(gin.Logger())
-
-	  routes.UserRoutes(router)
-	  router.Use(middleware.Authentication())
-
-	  router.GET("/addcart", app.AddToCart())
-	  router.GET("/removeiteam", app.RemoveIteam())
-	  router.GET("/cartcheckout", app.ByFromCart())
-	  router.GET("/instantrbuy", app.InstantBuy())
-
-	  log.Fatal(router.Run(":" + port))
-
-
 	}
+
+	app := controllers.NewAppliction(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addcart", app.AddToCart())
+	router.GET("/removeiteam", app.RemoveIteam())
+	router.GET("/cartcheckout", app.ByFromCart())
+	router.GET("/instantrbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+
+}
